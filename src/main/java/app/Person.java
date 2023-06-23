55bea2b33c0bb0a8473375283a2efc7ec8e22311
Rams200599 <136055767+Rams200599@users.noreply.github.com>
2023-06-23 02:23:01 -0400

    Update Person.java

--- src/main/java/app/Person.java
+++ src/main/java/app/Person.java
@@ -1,1 +1,45 @@
+package app;
 
+public class Person {
+    private String firstName = null;
+    private String lastName = null;
+    private String address = null;
+
+    public Person(String firstName, String lastName, String address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+    }
+
+    public Person() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Person [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + "]";
+    }
+}
